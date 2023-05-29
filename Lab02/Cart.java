4711a9b83bdcef2ce671025982fd44fdc34f2517
nmduonggg <nmduong2104@gmail.com>
2023-05-29 12:14:35 +0700

    fix bugs in lab02

--- Lab02/Cart.java
+++ Lab02/Cart.java
@@ -1,5 +1,42 @@
+import javax.lang.model.type.NullType;
 
 public class Cart {
 	public static final int MAX_NUMBERS_ORDERED = 20;
-	private DigitalVideoDics itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED])
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+	private int qtyOrdered;
+	private float cost;
+
+	public boolean addDigitalVideoDisc(DigitalVideoDisc disc) {
+		for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+			if ((itemsOrdered[i] == null) && (qtyOrdered < MAX_NUMBERS_ORDERED)) {
+				itemsOrdered[i] = disc;
+				qtyOrdered = qtyOrdered + 1;
+				return true;
+			}
+		} return false;
+	}
+
+	public boolean removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+			DigitalVideoDisc curr_disc = itemsOrdered[i];
+			if ((curr_disc != null) & (curr_disc.getCategory() == disc.getCategory()) && (curr_disc.getClass() == disc.getClass())
+				&& (curr_disc.getDirector() == disc.getDirector()) && (curr_disc.getLength() == curr_disc.getLength())
+				&& (curr_disc.getTitle() == disc.getTitle())) {
+					itemsOrdered[i] = null;
+					qtyOrdered = qtyOrdered - 1;
+					return true;
+				}
+		} return false;
+	}
+
+	public float totalCost() {
+		float res = (float)0.0;
+		for (int i = 0; i < MAX_NUMBERS_ORDERED; i++){
+			if (itemsOrdered[i] != null){
+				res = res + itemsOrdered[i].getCost();
+			}
+		}
+		return res;
+	}
 }
